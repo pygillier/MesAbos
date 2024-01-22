9d47101aa34e36216587d29542c06dbc01435ec0
Pierre-Yves Gillier <github@pygillier.me>
2024-01-22 22:51:02 +0100

    Alembic support for DB migrations.

--- backend/models/subscription.py
+++ backend/models/subscription.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Float, Date
 from datetime import date
 from . import Base
 from backend.schemas import subscription as schemas
@@ -8,10 +9,10 @@ class Subscription(Base):
     __tablename__ = "subscriptions"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
-    name: Mapped[str]
-    amount: Mapped[float]
-    status: Mapped[str] = mapped_column(default=schemas.Status.active)
-    billing: Mapped[str] = mapped_column(default=schemas.BillingMode.monthly)
+    name: Mapped[str] = mapped_column(String(255))
+    amount: Mapped[float] = mapped_column(Float)
+    status: Mapped[str] = mapped_column(String(10), default=schemas.Status.active)
+    billing: Mapped[str] = mapped_column(String(8), default=schemas.BillingMode.monthly)
     renewal_date: Mapped[date]
-    payment: Mapped[str]
-    tags: Mapped[str]
+    payment: Mapped[str] = mapped_column(String(12))
+    tags: Mapped[str] = mapped_column(String(1024))
